34c9eb17603236729604dcd0ea772f390180c99c
arthur-asilva <dev.arthur.silva@gmail.com>
2022-04-27 09:06:03 -0300

    add apps folder, tools folder with private file containing data for database connection and dict for name of access groups

--- lessons_program/settings.py
+++ lessons_program/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from tools.private_data import DATABASE_CONNECTION
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-jx1f6!!#f+n8!xhm7j&))9frv)%&8ep$al02)_dpo67f=*3%gp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tools',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +75,14 @@ WSGI_APPLICATION = 'lessons_program.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = DATABASE_CONNECTION
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
