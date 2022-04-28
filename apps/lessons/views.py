dfeb9f4061d8890aa154efe1f2ad84de4d14b8cd
arthur-asilva <dev.arthur.silva@gmail.com>
2022-04-28 17:29:52 -0300

    working in get patient models to use by advisor

--- apps/lessons/views.py
+++ apps/lessons/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from apps.user.models import User, Patient
 
 def lessons_dashboard(request):
+    print(request.session['auth_session'])
+    # patients = Patient.objects.get(advisor)
     return render(request, 'lessons/lessons.html')
